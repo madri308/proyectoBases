ae247f01455424d0dfe4e67f0b7c1752f69019d4
Esteban Madrigal <40669201+madri308@users.noreply.github.com>
2020-06-27 23:13:43 -0600

    Procesa pagos
    Ya esta procesa pagos terminado solo falta probarlo

--- alterSPscripts/alterSP_ProcesarPagos.sql
+++ alterSPscripts/alterSP_ProcesarPagos.sql
@@ -8,35 +8,60 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE PROC [dbo].[SP_ProcesarPagos] 
-@Pagos PagosTipo READONLY
+CREATE PROC [dbo].[SP_ProcesarPagos] @Pagos PagosTipo READONLY
 AS   
 	BEGIN
 		BEGIN TRY
 		SET NOCOUNT ON 
 		SET XACT_ABORT ON
+			DECLARE @idMenor INT, @idMayor INT, @idRecibo INT, @fechaVence DATE, @fechaOperacion DATE, @montoMoratorio MONEY,
+					@idComprobante INT, @tasaMoratoria FLOAT, @montoRecibo MONEY
+			SELECT @idMenor = min([id]), @idMayor=max([id]) FROM @Pagos
+			BEGIN TRAN
+				WHILE @idMenor<=@idMayor
+				BEGIN
+					--PAGA EL RECIBO MAS VIEJO PENDIENTE CORRESPONDIENTE AL CONCEPTO DE COBRO.
+					SET @idRecibo = (SELECT R.id 
+										FROM [dbo].[Recibos] R
+										INNER JOIN @Pagos P ON P.idTipoRecibo = R.id_CC
+										WHERE R.fecha = (SELECT min(fecha) FROM [dbo].[Recibos] WHERE estado = 0) 
+										AND P.id = @idMenor)
+					UPDATE [dbo].[Recibos]
+					SET [estado] = 1
+					WHERE [id] = @idRecibo
 
-			SELECT *
-			INTO #RecibosPendientes
-			FROM [dbo].[Recibos]
-			WHERE [dbo].[Recibos].[estado] = 0
+					SET @montoRecibo = (SELECT monto FROM [dbo].[Recibos] WHERE id = @idRecibo)
 
-			DECLARE @idRecibo INT
-			SET @idRecibo = (SELECT id FROM #RecibosPendientes WHERE fecha = min(fecha))
+					--VERIFICA SI SE DEBE CREAR RECIBO MORATORIO
+					SET @fechaVence = (SELECT fechaVence FROM [dbo].[Recibos] WHERE id = @idRecibo)
+					SET @fechaOperacion = (SELECT fechaOperacion FROM @Pagos WHERE id = @idMenor)
+					--------------Verifica si existe un comprobante para este dia y sino lo crea
+					SET @idComprobante = (SELECT id FROM [dbo].[ComprobantePago] WHERE fecha = @fechaOperacion)
+					IF @idComprobante IS NULL
+						INSERT INTO [dbo].[ComprobantePago](fecha,total)
+						SELECT @fechaOperacion,0
+					SET @idComprobante = (SELECT id FROM [dbo].[ComprobantePago] WHERE fecha = @fechaOperacion)
+					SET @montoMoratorio = 0
+					IF @fechaVence <= @fechaOperacion
+						SET @tasaMoratoria = (SELECT tasaImpuestoMoratorio FROM [dbo].[ConceptoDeCobro] CC INNER JOIN @Pagos P ON P.idTipoRecibo = CC.id)
+						SET @montoMoratorio = (@montoRecibo*@tasaMoratoria/365)*ABS(DATEDIFF(d,@fechaVence,@fechaOperacion))
+					
+						INSERT INTO [dbo].[Recibos](id_CC,id_Comprobante,monto,estado,id_Propiedad,fecha,fechaVence)
+						SELECT 11,@idComprobante,@montoMoratorio,1,PR.id,@fechaOperacion,DATEADD(d,CC.diasParaVencer,@fechaOperacion)
+						FROM @Pagos P
+						INNER JOIN [dbo].[Propiedad] PR ON P.numFinca = PR.numFinca
+						INNER JOIN [dbo].[ConceptoDeCobro] CC ON p.idTipoRecibo = CC.id
+					UPDATE [dbo].[ComprobantePago]
+					SET [total] = [total]+@montoRecibo+@montoMoratorio
+					FROM [dbo].[ComprobantePago] CP
+					INNER JOIN [dbo].[Recibos] R ON R.id_CC = CP.id
+					WHERE R.id = @idRecibo
 
-			UPDATE [dbo].[Recibos] 
-			SET [dbo].[Recibos].[estado] = 1
-			WHERE [dbo].[Recibos].[id_Propiedad] 
-				IN (SELECT [dbo].[Propiedad].[id] 
-					FROM [Propiedad] 
-					WHERE numFinca 
-						IN (SELECT numFinca FROM @Pagos WHERE Recibos.id_CC 
-																IN (SELECT id_CC FROM @Pagos)))
-						AND id = @idRecibo
-
-
+					SET @idMenor = @idMenor+1
+				END
 		END TRY
 		BEGIN CATCH
+			ROLLBACK TRAN;
 			THROW 6000, 'Error: No se ha podido crear el pago.',1;
 		END CATCH
 	END
