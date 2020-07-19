76efa2eed9495ddfe0a543d1fcb2e5377367765c
Esteban Madrigal <40669201+madri308@users.noreply.github.com>
2020-07-18 18:20:32 -0600

    EMPIEZA 3RA PARTE
    Se modificaron las tablas y se agregaron SPs para poder pagar recibos desde pagina web solo falta probarlo
    
    Hecho Pago de recibos

---
+++ alterSPscripts/alterSP_pagarRecibos.sql
@@ -0,0 +1,79 @@
+﻿USE [Progra]
+GO
+IF OBJECT_ID('[dbo].[SP_pagarRecibos]') IS NOT NULL
+BEGIN 
+    DROP PROC [dbo].[SP_pagarRecibos]  
+END 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+--[
+ --{ "id" : 2},
+ --{ "id" : 5}
+--]
+CREATE PROC [dbo].[SP_pagarRecibos] @inIdRecibos varchar(MAX)
+AS 
+	BEGIN 
+		BEGIN TRY 
+		SET NOCOUNT ON 
+		SET XACT_ABORT ON  
+			DECLARE @idMenor INT, @idMayor INT, @montoRecibo MONEY, 
+					@montoMoratorio MONEY,@fechaOperacion DATE, @montoTotal MONEY,
+					@fechaVence DATE, @tasaMoratoria FLOAT,@idPropiedad int
+			CREATE TABLE ##idRecibosPagar (id INT IDENTITY(1,1),idRecibo INT);
+			--GUARDA LOS IDS DE LOS RECIBOS QUE QUIERO PAGAR
+			INSERT INTO ##idRecibosPagar(idRecibo)
+			SELECT id
+			FROM OPENJSON(@inIdRecibos)
+			WITH(id INT)
+
+			SET @montoTotal = 0
+
+			SELECT @idMenor = MIN([id]), @idMayor=MAX([id]) FROM ##idRecibosPagar--SACA ID MAYOR Y MENOR PARA ITERAR LA TABLA
+			BEGIN TRAN
+				WHILE @idMenor<=@idMayor--RECORRE LOS RECIBOS
+				BEGIN
+					SET @montoMoratorio = 0
+					SET @fechaOperacion = GETDATE()
+					SET @idPropiedad = (SELECT id_Propiedad FROM [Recibos] R 
+											INNER JOIN ##idRecibosPagar idRP ON idRP.idRecibo = R.id
+											WHERE @idMenor = idRP.id)
+					SET @fechaVence = (SELECT fechaVence FROM [dbo].[Recibos] R
+										   INNER JOIN ##idRecibosPagar idRP ON idRP.idRecibo = R.id
+										   WHERE @idMenor = idRP.id)
+					SET @montoRecibo = (SELECT R.monto FROM [dbo].[Recibos] R
+											INNER JOIN ##idRecibosPagar idRP ON R.id = idRP.idRecibo
+											WHERE idRP.id = @idMenor)
+					IF @fechaVence < @fechaOperacion
+					BEGIN
+						--SACA LA TASA MORATORIA DEL RECIBO
+						SET @tasaMoratoria = (SELECT CC.tasaImpuestoMoratorio FROM [dbo].[ConceptoDeCobro] CC
+												INNER JOIN [dbo].[Recibos] R ON R.id_CC = CC.id 
+												INNER JOIN  ##idRecibosPagar idRP ON idRP.idRecibo = R.id
+												WHERE idRP.id = @idMenor)
+
+						--AQUI CAMBIA EL MONTO MORATORIO YA QUE SI SE DEBE CREAR RECIBO MORATORIO
+						SET @montoMoratorio = (@montoRecibo*@tasaMoratoria/365)*ABS(DATEDIFF(d,@fechaVence,@fechaOperacion))
+						
+						--CREA UN RECIBO DE TIPO MORATORIO
+						INSERT INTO [dbo].[Recibos](id_CC,monto,estado,id_Propiedad,fecha,fechaVence)
+						SELECT CC.id,@montoMoratorio,0,@idPropiedad,@fechaOperacion,DATEADD(d,CC.diasParaVencer,@fechaOperacion)
+						FROM [dbo].[ConceptoDeCobro] CC
+						WHERE CC.id = 11
+
+						--GUARDA ADEMAS LOS RECIBOS MORATORIOS A PAGAR
+						INSERT INTO ##idRecibosPagar(idRecibo)
+						SELECT IDENT_CURRENT('[dbo].[Recibos]')
+					END
+					SET @montoTotal += @montoMoratorio+@montoRecibo
+					SET @idMenor += 1
+				END
+			COMMIT
+			SELECT [id_CC],[monto],[fecha],[fechaVence] FROM [dbo].[Recibos] R
+			INNER JOIN ##idRecibosPagar RP ON R.id = RP.idRecibo
+		END TRY
+		BEGIN CATCH
+			THROW 92039, 'Error: no se ha podido calcular el total.',1
+		END CATCH;
+	END
